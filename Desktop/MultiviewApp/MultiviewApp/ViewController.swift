df7c9aa09231c70bd63b25581e76ca756eb8775c
Rhythm Mecwan <rhythm813mecwan@gmail.com>
2018-10-25 22:35:49 -0700

    Tabs working

--- Desktop/MultiviewApp/MultiviewApp/ViewController.swift
+++ Desktop/MultiviewApp/MultiviewApp/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tabButton: UIButton!
+    @IBOutlet weak var stateButton: UIButton!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var passwordLabel: UILabel!
+    @IBOutlet weak var NameLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
